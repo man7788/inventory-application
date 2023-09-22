6c856457466eace26bdc740b655206e74e717137
man7788 <jerman1841@gmail.com>
2023-09-23 07:04:21 +0800

    Update bodystyle controller

--- controllers/bodystyleController.js
+++ controllers/bodystyleController.js
@@ -1,14 +1,39 @@
 const BodyStyle = require("../models/bodyStyle");
+const Car = require("../models/car");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all bodystyle.
 exports.bodystyle_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BodyStyle list");
+  const allBodyStyle = await BodyStyle.find().sort({ name: 1 }).exec();
+
+  res.render("bodystyle_list", {
+    title: "BodyStyle List",
+    bodystyle_list: allBodyStyle,
+  });
 });
 
 // Display detail page for a specific bodystyle.
 exports.bodystyle_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: BodyStyle detail: ${req.params.id}`);
+  // Get details of bodystyle and all associated cars (in parallel)
+  const [bodyStyle, carsInBodyStyle] = await Promise.all([
+    BodyStyle.findById(req.params.id).exec(),
+    Car.find({ body_style: req.params.id }, "name manufacturer")
+      .populate("manufacturer")
+      .exec(),
+  ]);
+
+  if (bodyStyle === null) {
+    // No results.
+    const err = new Error("BodyStyle not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bodystyle_detail", {
+    title: "Body Style Detail",
+    bodystyle: bodyStyle,
+    bodystyle_cars: carsInBodyStyle,
+  });
 });
 
 // Display bodystyle create form on GET.
