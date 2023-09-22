d32b54418de21dd2a8b1b609c6db5b01d7f4cf72
man7788 <jerman1841@gmail.com>
2023-09-23 07:00:29 +0800

    Setup mongoose connection

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const catalogRouter = require("./routes/catalog"); //Import routes for "catalog"
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://admin:dllm0044@cluster0.mk3oruf.mongodb.net/car_inventory?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
