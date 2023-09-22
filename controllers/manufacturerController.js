f8b01c010b00c28d128a836773a9acc2ba3d623b
man7788 <jerman1841@gmail.com>
2023-09-23 07:04:04 +0800

    Update manufacturer controller

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,14 +1,39 @@
 const Manufacturer = require("../models/manufacturer");
+const Car = require("../models/car");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all manufacturer.
 exports.manufacturer_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Manufacturer list");
+  const allManufacturers = await Manufacturer.find().sort({ name: 1 }).exec();
+
+  res.render("manufacturer_list", {
+    title: "Manufacturer List",
+    manufacturer_list: allManufacturers,
+  });
 });
 
 // Display detail page for a specific manufacturer.
 exports.manufacturer_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Manufacturer detail: ${req.params.id}`);
+  // Get details of manufacturer and all their cars (in parallel)
+  const [manufacturer, allCarsByManufacturer] = await Promise.all([
+    Manufacturer.findById(req.params.id).exec(),
+    Car.find({ manufacturer: req.params.id }, "name body_style")
+      .populate("body_style")
+      .exec(),
+  ]);
+
+  if (manufacturer === null) {
+    // No results.
+    const err = new Error("Car not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("manufacturer_detail", {
+    title: "Manufacturer Detail",
+    manufacturer: manufacturer,
+    manufacturer_cars: allCarsByManufacturer,
+  });
 });
 
 // Display manufacturer create form on GET.
